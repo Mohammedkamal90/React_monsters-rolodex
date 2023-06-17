45ebda13d56260feb3ec31ead929bcdc34e3024f
MohammedKamal90 <mohammed.kamal14590@gmail.com>
2023-06-17 08:58:31 +0300

    rolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import "./card-list.style.css";
+import Card from "../card/card.component";
+
+const cardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </div>
+);
+export default cardList;
